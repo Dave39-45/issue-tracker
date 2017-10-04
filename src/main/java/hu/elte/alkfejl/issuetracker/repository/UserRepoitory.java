b15578a00469b37a9c138111dca2529c7f1711ce
Kovács Dávid <wdavew@INF.ELTE.HU>
2017-10-04 09:51:37 +0200

    first commit

---
+++ src/main/java/hu/elte/alkfejl/issuetracker/repository/UserRepoitory.java
@@ -0,0 +1,17 @@
+package hu.elte.alkfejl.issuetracker.repository;
+
+import hu.elte.alkfejl.issuetracker.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepoitory extends CrudRepository<User, String> {
+    //Ha nincs, akkor null -ad (Optional)
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
+}
